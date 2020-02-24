cb023039664adaa99b1e76aa1cb91782ca456d2b
ssjsrgx <ssjsrgx@gmail.com>
2020-02-24 23:59:14 +0100

    cositas

--- P1/src/es/pe/mdelossgavil/Main.java
+++ P1/src/es/pe/mdelossgavil/Main.java
@@ -15,32 +15,41 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		AlgoritmoGenetico a_genetico = new AlgoritmoGenetico(TAM_POB, N_GENERACIONES,false);
-		double[] x = new double[N_GENERACIONES];
-		double[] y = new double[N_GENERACIONES];
+		AlgoritmoGenetico a_genetico = new AlgoritmoGenetico(TAM_POB, N_GENERACIONES,true);
+		double[] iteraciones = new double[N_GENERACIONES];
+		double[] graficaMejorAbs = new double[N_GENERACIONES];
+		double[] graficaMedia = new double[N_GENERACIONES];
+		double[] graficaMejorRelativo = new double[N_GENERACIONES];
+
+		
 		Ruleta ruleta = new Ruleta();
 		Monopunto cruce = new Monopunto();
 		MutacionBoolean mutacion = new MutacionBoolean();
 
-		a_genetico.inicializa(ruleta, cruce, mutacion, "F2");
+		a_genetico.inicializa(ruleta, cruce, mutacion, "F1");
 		a_genetico.evaluar_poblacion();
 
 		int i = 0;
 		while (i < N_GENERACIONES) {
-			x[i] = i;
+			iteraciones[i] = i;
 			a_genetico.seleccion();
-			// System.out.println(a_genetico.get_aptitud_media());
 			a_genetico.cruce();
 			a_genetico.mutacion();
 			a_genetico.evaluar_poblacion();
-			y[i] = a_genetico.mejor_abs.get_aptitud();
-
+			
+			//Graficas
+			graficaMejorAbs[i] = a_genetico.mejor_abs.get_aptitud();
+			graficaMedia[i] = a_genetico.get_aptitud_media();
+			graficaMejorRelativo[i] = a_genetico.getEl_mejor().get_aptitud();
 			i++;
 		}
 
 		Grafica grafica = new Grafica(600, 600);
 		grafica.inicializa_grafica();
-		grafica.agregar_linea("lineaUno", x, y);
+		grafica.agregar_linea("Mejor Absoluto", iteraciones, graficaMejorAbs);
+		grafica.agregar_linea("Media", iteraciones, graficaMedia);
+		grafica.agregar_linea("Mejor de cada generación", iteraciones, graficaMejorRelativo);
+		
 		grafica.pinta_grafica();
 		System.out.println("EL MILL0R: " + a_genetico.mejor_abs.get_aptitud());
 
